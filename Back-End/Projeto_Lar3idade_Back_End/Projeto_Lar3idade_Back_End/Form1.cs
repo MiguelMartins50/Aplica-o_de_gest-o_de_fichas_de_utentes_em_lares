834e337fd62adc9f7890249a40ae6788eeca4114
MiguelMartins <a44610@alunos.ipb.pt>
2023-12-05 16:56:20 +0000

    login-basico

--- Back-End/Projeto_Lar3idade_Back_End/Projeto_Lar3idade_Back_End/Form1.cs
+++ Back-End/Projeto_Lar3idade_Back_End/Projeto_Lar3idade_Back_End/Form1.cs
@@ -12,9 +12,21 @@ namespace Projeto_Lar3idade_Back_End
 {
     public partial class Form1 : Form
     {
+       
         public Form1()
         {
             InitializeComponent();
+            string BluePath = System.IO.Path.Combine(Application.StartupPath, "azul2.png");
+            panel1.BackgroundImage = Image.FromFile(BluePath);
+            string logoPath = System.IO.Path.Combine(Application.StartupPath, "5857e68f1800001c00e435b3.jpeg");
+            panel2.BackgroundImage = Image.FromFile(logoPath);
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Back-End/Projeto_Lar3idade_Back_End/Projeto_Lar3idade_Back_End/Program.cs
+++ Back-End/Projeto_Lar3idade_Back_End/Projeto_Lar3idade_Back_End/Program.cs
@@ -17,6 +17,7 @@ namespace Projeto_Lar3idade_Back_End
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
